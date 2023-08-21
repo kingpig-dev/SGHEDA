bfd422842b8c1196c8996e87740ecef377220368
kingpig-dev <rowelyle9@gmail.com>
2023-08-21 09:19:11 +0900

    complete license algorithm

--- License/license.py
+++ License/license.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QIcon, QCursor
 from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QApplication, QWidget, QHBoxLayout, \
-    QMainWindow
+    QMainWindow, QGroupBox, QComboBox
 from labelclass import IntroLabel3, ImageButton
 from cryptography.fernet import Fernet
 import pyperclip
@@ -84,7 +85,7 @@ class Myapp(QMainWindow):
             border: none;
             font-size: 16px;
         ''')
-        self.encryption_key = b'-EDDWkBVgJ_O-dKDDlxbkMCgf322qlW_lYUj2JI2ExU='
+        self.encryption_key = 5
         self.state = 0
 
         self.resize(610, 250)
@@ -96,7 +97,7 @@ class Myapp(QMainWindow):
         self.label_title = IntroLabel3(self)
         self.label_title.setText('License Information')
         self.label_title.setAlignment(Qt.AlignCenter)
-        self.label_title.setGeometry(250, 50, 200, 30)
+        self.label_title.setGeometry(250, 35, 200, 30)
 
         self.label_machinenumber = QLabel(self)
         self.label_machinenumber.setText("Machine Number")
@@ -116,8 +117,50 @@ class Myapp(QMainWindow):
                                         border-bottom: 2px solid #1F8EFA;
                                     }
                                 ''')
-        self.label_machinenumber.setGeometry(180, 85, 130, 40)
-        self.machinenumber.setGeometry(320, 90, 270, 30)
+        self.label_machinenumber.setGeometry(180, 65, 130, 40)
+        self.machinenumber.setGeometry(320, 70, 270, 30)
+
+        self.groupbox = QGroupBox(self)
+        self.groupbox.setTitle('License Type')
+
+        self.combobox_selection = QComboBox(self.left_widget)
+        self.icon_design = QIcon('./Images/design.png')
+        self.icon_analysis = QIcon('./Images/analysis02.png')
+        self.combobox_selection.addItem(self.icon_design, ' Design')
+        self.combobox_selection.addItem(self.icon_analysis, 'Analysis ')
+        self.combobox_selection.resize(100, 30)
+        self.combobox_selection.setCursor(QCursor(Qt.PointingHandCursor))
+        self.combobox_selection.setStyleSheet(""" 
+                     QComboBox {
+                        color: #7C8AA7;
+                        background-color: #2C3751;
+                        selection-background-color: white;
+                        padding: 1px 1px 1px 1px;
+                        min-width: 2em;
+                        font-size: 16px;
+                    }
+
+                    QComboBox:hover {
+                        color: #2978FA;
+                    }
+
+                    QComboBox::drop-down {
+                        subcontrol-origin: padding;
+                        color: white;
+                        width: 10px;
+                        border: none;
+                    }
+
+                    QComboBox::down-arrow {
+                        border: 0px;
+                        background-image-width: 30px;
+                        border-image: url(./Images/down.png);
+                    }
+                """
+                                              )
+        self.combobox_selection.currentIndexChanged.connect(self.combobox_selection_changed)
+        self.combobox_selection.move(20, 155)
+
 
         self.label_serialnumber = QLabel(self)
         self.label_serialnumber.setText('Serial Number')
@@ -134,13 +177,12 @@ class Myapp(QMainWindow):
                                        background-color: #1F2843;
                                        border: none;
                                        border-bottom: 2px solid #1F8EFA;
-                                       font-size: 14px;
                                    }
                                ''')
-        self.label_serialnumber.setGeometry(180, 130, 130, 30)
-        self.serialnumber.setGeometry(320, 130, 270, 30)
+        self.label_serialnumber.setGeometry(180, 160, 130, 30)
+        self.serialnumber.setGeometry(320, 160, 270, 30)
         self.serialnumber.setReadOnly(True)
-        self.serialnumber.mousePressEvent()
+        # self.serialnumber.mousePressEvent()
 
         self.btn_confirm = QPushButton(self)
         self.btn_confirm.setText('Generate')
@@ -160,7 +202,7 @@ class Myapp(QMainWindow):
                             background-color: #5D7C4C;
                         }
                     """)
-        self.btn_confirm.setGeometry(280, 180, 130, 40)
+        self.btn_confirm.setGeometry(280, 200, 130, 40)
         self.btn_confirm.clicked.connect(self.generate_license)
 
     def generate_license(self):
@@ -170,17 +212,55 @@ class Myapp(QMainWindow):
             print("machine number: ", machine_number)
 
             # get use number
-            num_design = str(0)
-            num_analysis = str(0)
-            data = machine_number + num_design + num_analysis
+            num_design = '{:4d}'.format(45).replace(' ', '0')
+            num_analysis = '{:4d}'.format(45).replace(' ', '0')
+            data = num_design + machine_number[8:16] + num_analysis + machine_number[0:8]
 
             # encrypt data with key
-            cipher_suite = Fernet(self.encryption_key)
-            self.encrypted_data = cipher_suite.encrypt(data.encode())
-
-            self.serialnumber.setText(self.encrypted_data.decode())
+            self.encrypted_data = self.caesar_encrypt(data, self.encryption_key)
+            self.serialnumber.setText(self.encrypted_data)
             self.state = 1
 
+            # decrypt data with key
+            self.decrypted_data = self.caesar_decrypt(self.encrypted_data, self.encryption_key)
+
+    def caesar_encrypt(self, plaintext, shift):
+        ciphertext = ""
+        for char in plaintext:
+            # Encrypt uppercase letters
+            if char.isupper():
+                encrypted_char = chr((ord(char) - 65 + shift) % 26 + 65)
+            # Encrypt lowercase letters
+            elif char.islower():
+                encrypted_char = chr((ord(char) - 97 + shift) % 26 + 97)
+            # digit
+            elif char.isdigit():
+                encrypted_char = chr((ord(char) - 48 + shift) % 10 + 48)
+            else:
+                # Leave non-alphabetic characters unchanged
+                encrypted_char = char
+            ciphertext += encrypted_char
+        print("ciphertext", ciphertext)
+        return ciphertext
+
+    def caesar_decrypt(self, ciphertext, shift):
+        plaintext = ""
+        for char in ciphertext:
+            # Decrypt uppercase letters
+            if char.isupper():
+                decrypted_char = chr((ord(char) - 65 - shift) % 26 + 65)
+            # Decrypt lowercase letters
+            elif char.islower():
+                decrypted_char = chr((ord(char) - 97 - shift) % 26 + 97)
+            # Decrypt digit
+            elif char.isdigit():
+                decrypted_char = chr((ord(char) - 48 - shift) % 10 + 48)
+            else:
+                # Leave non-alphabetic characters unchanged
+                decrypted_char = char
+            plaintext += decrypted_char
+        print("plaintext: ", plaintext)
+        return plaintext
     def copy_clipboard(self):
         print('copy clipboardd')
 
